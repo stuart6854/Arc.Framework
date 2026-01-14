8273bae26cafbf5aea44d35d4ba6e8356e5e52ec
Stuart Millman <stu.millman15@gmail.com>
2026-01-14 11:42:44 +0000

    add(Arc.Core): RandomExtensions

---
+++ Source/Arc.Core/Source/Extensions/RandomExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Arc.Core.Extensions;
+
+public static class RandomExtensions
+{
+    extension(Random random)
+    {
+        public float NextNormalizedSingle() => random.NextSingle() * 2 - 1;
+        public double NextNormalizedDouble() => random.NextDouble() * 2 - 1;
+    }
+}
